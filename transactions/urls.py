9c43018824bc41c681c9f6415a1aacd55a5035c8
Raiyan <tajkierhaque@gmail.com>
2023-12-28 12:41:03 +0600

    transaction report view done

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import DepositMoneyView, WithdrawView, LoanRequestView
+from .views import DepositMoneyView, WithdrawView, LoanRequestView, TransactionReportView
 
-# app_name = 'transactions'
+
 urlpatterns = [
     path("deposit/", DepositMoneyView.as_view(), name="deposit"),
     path("withdraw/", WithdrawView.as_view(), name="withdraw"),
     path("loan-request/", LoanRequestView.as_view(), name="loan_request"),
+    path("report/", TransactionReportView.as_view(),
+         name="transaction_report"),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,20 +1,24 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import TransactionModel
-from django.views.generic import CreateView
-from .constants import DEPOSIT, WITHDRAWAL, LOAN, LOAN_PAID
+from django.views.generic import CreateView, ListView
+from .constants import DEPOSIT, WITHDRAWAL, LOAN
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import TransactionForm, DepositForm, WithdrawForm, LoanRequestForm
+from .forms import DepositForm, WithdrawForm, LoanRequestForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http import HttpResponse
+from datetime import datetime
+from django.utils import timezone
+from django.db.models import Sum
 # Create your views here.
 
 
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = "transactions/transaction_form.html"
     model = TransactionModel
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("transaction_report")
     title = ""
 
     def get_form_kwargs(self):
@@ -92,7 +96,7 @@ class LoanRequestView(TransactionCreateMixin):
         amount = form.cleaned_data.get('amount')
         account = self.request.user.account
         loan_count = TransactionModel.objects.filter(
-            account=account, transaction_type=LOAN, loan_approved=True).count()
+            account=account, transaction_type=LOAN, loan_approve=True).count()
 
         if loan_count >= 3:
             return HttpResponse("You have cross the loan limits")
@@ -103,3 +107,39 @@ class LoanRequestView(TransactionCreateMixin):
                 float(amount))}$ submitted successfully'
         )
         return super().form_valid(form)
+
+
+class TransactionReportView(LoginRequiredMixin, ListView):
+    template_name = "transactions/transaction_report.html"
+    title = "Transactions"
+    balance = 0
+    model = TransactionModel
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(account=self.request.user.account)
+
+        start_date_str = self.request.GET.get('start_date')
+        end_date_str = self.request.GET.get('end_date')
+
+        if start_date_str and end_date_str:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+
+            queryset = queryset.filter(
+                timestamp__date__gte=start_date, timestamp__date__lte=end_date)
+
+            self.balance = TransactionModel.objects.filter(
+                timestamp__date__gte=start_date, timestamp__date__lte=end_date
+            ).aggregate(Sum('amount'))['amount__sum']
+        else:
+            self.balance = self.request.user.account.balance
+
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'account': self.request.user.account
+        })
+
+        return context
